1751833bbaaf5e82955a4ef9ad65b57a447e240c
ivaaner <ivan.ernandorena@leapsight.com>
2019-02-19 09:05:28 -0300

    flatten seq expr

--- dead-code.rkt
+++ dead-code.rkt
@@ -24,15 +24,27 @@
 )
 
 
-(define (bury expr o)
+(define (do-bury expr o)
   (match expr
     [(Skip) (Skip)]
     [(Assign x a) (cond [(set-member? o x) (Assign x a)] [else (Skip)])]
-    [(Seq s) #:when (> (length s) 1) (Seq (list (bury (first s) (live (Seq (rest s)) o)) (bury (Seq (rest s)) o)))]
-    [(Seq s) #:when (= (length s) 1) (bury (first s) o)]
-    [(If cnd thn els) (If cnd (bury thn o) (bury els o))]
-    [(While cnd body) (While cnd (bury body (live (While cnd body) o)))]
+    [(Seq s) #:when (> (length s) 1) (Seq (list (do-bury (first s) (live (Seq (rest s)) o)) (do-bury (Seq (rest s)) o)))]
+    [(Seq s) #:when (= (length s) 1) (do-bury (first s) o)]
+    [(If cnd thn els) (If cnd (do-bury thn o) (do-bury els o))]
+    [(While cnd body) (While cnd (do-bury body (live (While cnd body) o)))]
   )
+)
+
+(define (aplanar prog)
+  (match prog
+    [(Seq s) (Seq (flatten (map (lambda (elem) (match elem [(Seq s1) s1] [else elem])) s)))]
+    [(If cnd thn els) (If cnd (aplanar thn) (aplanar els))]
+    [(While cnd body) (While cnd (aplanar body))]
+    [else prog]))
+
+(define (bury expr o)
+  (define dead-code (do-bury expr o))
+  (aplanar dead-code)
 )
 
 (module+ test
@@ -43,7 +55,7 @@
                  }))
   
   (define test-stmt-2
-    (parse-stmt '{if {> x 4} {{:= y 2}{:= y 10}} {{:= x 1}{:= x 10}}}))
+    (parse-stmt '{if {> x 4} {{:= y 2}{:= y 10}} {{:= x 1}{:= x 10}{:= x 1}}}))
 
   (define test-stmt-3
      (parse-stmt '{{:= x 3}{:= x 2}{:= x 1}}))
@@ -60,11 +72,11 @@
 
  (printf "Test 2\n")
  (define test2 (bury test-stmt-2 (set 'x 'y)))
- (check-equal? test2 (If (Greater 'x 4) (Seq (list (Skip) (Assign 'y 10))) (Seq (list (Skip) (Assign 'x 10)))))
+ (check-equal? test2 (If (Greater 'x 4) (Seq (list (Skip) (Assign 'y 10))) (Seq (list (Skip) (Skip) (Assign 'x 1)))))
 
  (printf "Test 3\n")
  (define test3 (bury test-stmt-3 (set 'x)))
- (check-equal? test3 (Seq (list (Skip) (Assign 'x 1))))
+ (check-equal? test3 (Seq (list (Skip) (Skip) (Assign 'x 1))))
 
  (printf "Test 4\n")
  (define test4 (bury test-stmt-4 (set 'x 'y)))
