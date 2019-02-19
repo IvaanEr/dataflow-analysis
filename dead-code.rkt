1db785046908bebe45d08f794ac8110dffa1ccec
ivaaner <ivan.ernandorena@leapsight.com>
2019-02-18 21:56:59 -0300

    dead-code finished

--- dead-code.rkt
+++ dead-code.rkt
@@ -20,8 +20,6 @@
   (define ll (hash->list live-IN))
   (filter (lambda (elem) (is-first (car elem))) ll)
   (define conjunto (cdr (first ll)))
-  ;(print live-IN)
-  ;(print conjunto)
   conjunto
 )
 
@@ -37,16 +35,17 @@
 )
 
 (module+ test
+
+  (define test-stmt-1
+    (parse-stmt '{while {> x 5}
+                        {if {> y 10} {:= x {+ x 1}} {{:= y {- y 1}}{:= y 10}}}
+                 }))
   
   (define test-stmt-2
-    (parse-stmt '{{:= x 1}
-                  {:= x 5}
-                  {if {> x 6} {:= y x} {:= y 10}}
-                 }))
+    (parse-stmt '{if {> x 4} {{:= y 2}{:= y 10}} {{:= x 1}{:= x 10}}}))
 
-  (print test-stmt-2)
   (define test-stmt-3
-     (parse-stmt '{:= x 1}))
+     (parse-stmt '{{:= x 2}{:= x 1}}))
 
   (define test-stmt-4
     (parse-stmt '{while {> x 1}
@@ -55,14 +54,17 @@
                         }
                  }))
 
+ (define test1 (bury test-stmt-1 (set 'x 'y)))
+ (check-equal? test1 (While (Greater 'x 5) (If (Greater 'y 10) (Assign 'x (Plus 'x 1)) (Seq (list (Skip) (Assign 'y 10))))))
+  
  (define test2 (bury test-stmt-2 (set 'x 'y)))
-  (print test2)
+ (check-equal? test2 (If (Greater 'x 4) (Seq (list (Skip) (Assign 'y 10))) (Seq (list (Skip) (Assign 'x 10)))))
   
  (define test3 (bury test-stmt-3 (set 'x)))
- (check-equal? test3 (Assign 'x 1))
+ (check-equal? test3 (Seq (list (Skip) (Assign 'x 1))))
 
  (define test4 (bury test-stmt-4 (set 'x 'y)))
  (check-equal? test4 (While (Greater 'x 1) (Seq (list (Assign 'x (Plus 'x 1)) (Skip)))))
-
+ 
 )
 
